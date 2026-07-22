125142ffa47bda6b772fc5d212d215dab9bff350
GitHub git update Action <action@github.com>
2026-07-22 01:15:45 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_mega_stm32f4xx.h
+++ generated/controllers/generated/rusefi_generated_mega_stm32f4xx.h
@@ -1629,7 +1629,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2362468840
+#define SIGNATURE_HASH 1781926454
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2269,7 +2269,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.07.21.mega_stm32f4xx.2362468840"
+#define TS_SIGNATURE "rusEFI main.2026.07.22.mega_stm32f4xx.1781926454"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_mega_stm32f4xx.h
+++ generated/controllers/generated/signature_mega_stm32f4xx.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2362468840
-#define TS_SIGNATURE "rusEFI main.2026.07.21.mega_stm32f4xx.2362468840"
+#define SIGNATURE_HASH 1781926454
+#define TS_SIGNATURE "rusEFI main.2026.07.22.mega_stm32f4xx.1781926454"
