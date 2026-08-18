9b126f4a3d4cfc6f44d757735c1733406274bfbf
GitHub git update Action <action@github.com>
2026-08-18 00:28:59 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_mega_stm32f4xx.h
+++ generated/controllers/generated/signature_mega_stm32f4xx.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3192948667
-#define TS_SIGNATURE "rusEFI main.2026.08.17.mega_stm32f4xx.3192948667"
+#define SIGNATURE_HASH 2073713542
+#define TS_SIGNATURE "rusEFI main.2026.08.18.mega_stm32f4xx.2073713542"
