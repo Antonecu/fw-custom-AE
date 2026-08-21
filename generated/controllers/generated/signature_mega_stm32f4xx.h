81ac03860f1166bda74a5c15a611357e40135290
GitHub git update Action <action@github.com>
2026-08-21 00:31:29 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_mega_stm32f4xx.h
+++ generated/controllers/generated/signature_mega_stm32f4xx.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 795493949
-#define TS_SIGNATURE "rusEFI main.2026.08.20.mega_stm32f4xx.795493949"
+#define TS_SIGNATURE "rusEFI main.2026.08.21.mega_stm32f4xx.795493949"
